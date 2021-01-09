76e46264778a7f91de76bcbf7abdaf171c0355e3
Tiago Coelho <Tiagocoelho33@gmail.com>
2021-01-08 21:01:24 -0300

    init commit. Xcode project created

---
+++ ChuckNorrisApp/ChuckNorrisApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorrisApp
+//
+//  Created by Tiago Coelho on 08/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
